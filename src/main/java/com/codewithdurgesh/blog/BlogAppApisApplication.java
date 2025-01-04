ffc252399a5ccc7fd6f915a0c51534de899b0271
User <User@DESKTOP-E3TQOID>
2025-01-05 01:57:14 +0530

    Add Logger

--- src/main/java/com/codewithdurgesh/blog/BlogAppApisApplication.java
+++ src/main/java/com/codewithdurgesh/blog/BlogAppApisApplication.java
@@ -1,5 +1,10 @@
 package com.codewithdurgesh.blog;
 
+//import org.apache.log4j.Appender;
+//import org.apache.log4j.ConsoleAppender;
+//import org.apache.log4j.Layout;
+//import org.apache.log4j.Logger;
+//import org.apache.log4j.SimpleLayout;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +12,19 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogAppApisApplication {
+	
+//	private static Logger logger = Logger.getLogger(BlogAppApisApplication.class);//step1: Create object of logger using various impl
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
+		
+		//Step2: Creating the object os Layout[How to show the logg's]
+//		Layout layout = new SimpleLayout();
+		
+		//Step3: Where to store logs
+//		Appender appender = new ConsoleAppender(layout);
+//		logger.addAppender(appender);
+		
 	}
 	
 	@Bean
--- src/main/java/com/codewithdurgesh/blog/controllers/UserController.java
+++ src/main/java/com/codewithdurgesh/blog/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.codewithdurgesh.blog.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,40 +26,54 @@ import com.codewithdurgesh.blog.services.UserService;
 @RequestMapping("/api/users")
 public class UserController {
 
+	private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserService userService;
-	
-	//POST-create user
+
+	// POST-create user
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
 		UserDto createUserDto = this.userService.createUser(userDto);
-		return new ResponseEntity<>(createUserDto,HttpStatus.CREATED);
-		
+		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+
 	}
-	
-	//PUT-update user
+
+	// PUT-update user
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId") Integer uid){
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
 		UserDto updateUser = this.userService.updateUser(userDto, uid);
 		return ResponseEntity.ok(updateUser);
-		
+
 	}
-	
-	//DELETE-delete user
+
+	// DELETE-delete user
 	@DeleteMapping("/{userId}")
-	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid){
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid) {
 		this.userService.deleteUser(uid);
-		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully",true),HttpStatus.OK);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully", true), HttpStatus.OK);
 	}
-	
-	//GET-user get
+
+	// GET-user get
 	@GetMapping("/")
-	public ResponseEntity<List<UserDto>> getAllUsers(){
-		return ResponseEntity.ok(this.userService.getAllUsers());
+	public ResponseEntity<List<UserDto>> getAllUsers() {
+		String sMsg="Start point of getAllUsers()";
+		System.out.println(LocalDate.now()+""+sMsg);
+		logger.info("Received request to fetch all users");
+		logger.info(sMsg);
+		List<UserDto> users = this.userService.getAllUsers();
+		String eMsg = "Exit point of getAllUsers()";
+		System.out.println(LocalDate.now()+""+eMsg);
+		logger.info("Fetched {} users successfully", users.size());
+		logger.info(eMsg);
+		return ResponseEntity.ok(users);
+//	public ResponseEntity<List<UserDto>> getAllUsers(){
+//		logger.info("Received request to fetch all users");
+//		return ResponseEntity.ok(this.userService.getAllUsers());
 	}
-	
+
 	@GetMapping("/{userId}")
-	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId){
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId) {
 		return ResponseEntity.ok(this.userService.getUserById(userId));
 	}
 }
